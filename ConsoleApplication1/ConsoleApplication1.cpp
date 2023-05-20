b138ce92de508046eec04ffffeec1c239d78f08d
Elice <elicekolyakova@gmail.com>
2023-05-20 12:32:30 +0300

    lab_3 init

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	 
+	string s = "comfortable";
+	string ss = "";
+
+	string rest;
+	int i = 0;
+	cout << s << endl << endl;
+	do
+	{
+		i = s.find(ss, i);	if (i == -1) break;
+		cout << "index " << i << endl;
+		rest.assign(s, i, s.size());
+		cout << rest << endl << endl;
+		++i;
+	} while (i != -1);
+
+	return 0;
+}
---
+++ Lab2.1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab2._1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Technical_Work(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sorry, this feature is temporarily unavailable, technical work is underway...");
+        }
+
+        private void Exit(object sender, RoutedEventArgs ex)
+        {
+            this.Close();
+        }
+
+        private void Connect_Author(object sender, RoutedEventArgs Author)
+        {
+            MessageBox.Show("You could connect with author by:\nVK: @design71123\nE-mail: 77707cx@gmail.com");
+        }
+
+        private void About(object sender, RoutedEventArgs about)
+        {
+            MessageBox.Show("Автор приложения: Лушин Владимир Айдарович, студент 1 курса институ передовых информационных технологий ТГПУ Л. Н. Толстого.");
+        }
+
+        private void Fun(object sender, RoutedEventArgs fun)
+        {
+            MessageBox.Show("Just for FUN...");
+        }
+    }
+}
